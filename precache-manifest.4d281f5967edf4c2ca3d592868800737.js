46d44ab3305ffcbf1d4d5c56afbd1934afde9628
Prithvi <sannyasingal@gmail.com>
2020-07-26 16:25:10 +0530

    Updates

---
+++ precache-manifest.4d281f5967edf4c2ca3d592868800737.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04474c68991018568a5d9928bd77d8dc",
+    "url": "/puran-herbal/index.html"
+  },
+  {
+    "revision": "6ccc95bca877b1d4fc3c",
+    "url": "/puran-herbal/static/css/main.ca7b0b65.chunk.css"
+  },
+  {
+    "revision": "b6a6127d5e0c59e902c6",
+    "url": "/puran-herbal/static/js/2.4f0110ae.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/puran-herbal/static/js/2.4f0110ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ccc95bca877b1d4fc3c",
+    "url": "/puran-herbal/static/js/main.231cf8c3.chunk.js"
+  },
+  {
+    "revision": "f54d669c73ab1bc33741",
+    "url": "/puran-herbal/static/js/runtime-main.b0346153.js"
+  },
+  {
+    "revision": "710f4746e6d0d01112df9892bf072505",
+    "url": "/puran-herbal/static/media/hand_wash.710f4746.svg"
+  }
+]);
